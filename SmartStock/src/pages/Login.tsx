6034c09a5baae9e3cb125dc788f62bb10173d20d
tamitooo <tamy.flores@utec.edu.pe>
2025-05-10 00:21:00 -0500

    login 90% y registro 60%

--- SmartStock/src/pages/Login.tsx
+++ SmartStock/src/pages/Login.tsx
@@ -1,42 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
- const Login: React.FC = () => {
+const Login: React.FC = () => {
   return (
-    <div className='flex flex-col justify-center items-center'>
-      
-
-      <div className="flex w-full">
-        <div className="w-full flex items-center justify-center lg:w-1/2">
-          <div className='px-10 py-20 rounded-lg backdrop-blur bg-white/50 shadow-xl'>
-            <h1 className="font-extrabold text-center text-5xl">
-              Welcome Back
+    <div className="flex justify-center items-center h-screen bg-primary">
+      <div className="flex w-full justify-center">
+        <div className="w-[650px] flex items-center justify-center">
+          <div className="w-full h-[650px] p-10 bg-primary shadow-2xl shadow-black/60">
+            <h1 className="font-Judson font-bold text-center text-[96px] text-[#FCF7D5]">
+              Welcome!
             </h1>
-            <p className='text-center font-medium text-lg text-gray-500 mt-4'>
-              Welcome back! Please enter your details.
-            </p>
+    
 
             <div className="mt-8">
               <div className="mb-4">
-                <label className='px-4 text-lg font-medium'>Email</label>
+                <label className=" ml-12 font-Judson block text-[36px] font-medium text-white mb-1">Email</label>
+                <div className='flex items-center justify-center'>
                 <input 
-                  className="w-full rounded-lg p-2 mt-1 bg-transparent shadow-lg"
+                  className=" w-[500px] p-3 bg-[#ADBAC7] text-white font-Judson italic placeholder-gray-700 rounded-3xl"
                   placeholder="Enter your email"
                   type="email"
-                />
+                /></div>
               </div>
-              <div className="mb-4">
-                <label className='px-4 text-lg font-medium'>Password</label>
+              <div className="mb-6">
+                <label className=" ml-12 font-Judson block text-[36px] font-medium text-white mb-1">Password</label>
+                <div className='flex items-center justify-center'>
                 <input 
-                  className="w-full rounded-lg p-2 mt-1 bg-transparent shadow-lg"
+                  className="w-[500px] p-3 bg-[#ADBAC7] text-white font-Judson italic placeholder-gray-700 rounded-3xl"
                   placeholder="Enter your password"
                   type="password"
-                />
+                /></div>
               </div>
 
-              <div className="flex justify-center items-center">
+              <div className="flex justify-center ">
                 <button
-                  className="cursor-pointer w-1/2 bg-primary text-white py-2 px-4 rounded-lg font-semibold shadow-lg shadow-primary/50"
+                  className="w-[100px] h-12 bg-[#F7E67C] py-1 mt-5 text-[25px] text-primary font-suranna shadow-lg transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:bg-[#488ED3] duration-300 rounded-3xl"
                   type="submit"
                 >
                   Log In
@@ -45,11 +43,9 @@ import { Link } from 'react-router-dom';
             </div>
           </div>
         </div>
-
-        <div className="hidden lg:flex h-full w-1/2 pt-20 items-center justify-center bg-gray-200">
-        </div>
       </div>
     </div>
   );
 };
+
 export default Login;
--- SmartStock/src/pages/Register.tsx
+++ SmartStock/src/pages/Register.tsx
@@ -1,98 +1,72 @@
-import React, { useState} from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+
 
  const Register: React.FC = () => {
-  const [activeButton, setActiveButton] = useState<string | null>(null);
-
-  const handleButtonClick = (buttonType: string) => {
-    setActiveButton(buttonType);
-    
-  };
- 
 
   return (
-    <div className='flex flex-col justify-center items-center'>
-      <div className='pt-8 pb-8 flex'>
-        <div className='px-2'>
-          <div className='cursor-pointer w-1/2 py-2 px-4 w-24 rounded-full bg-transparent drop-shadow-md'>
-            <Link to="/login">
-              <div className='font-extrabold text-center'>Log In</div>
-            </Link>
-          </div>
-        </div>
-        <div className='px-2'>
-          <div className='cursor-pointer w-1/2 py-2 px-4 w-24 rounded-full bg-transparent border-2 border-primary drop-shadow-md'>
-            <Link to="/register">
-              <div className='font-extrabold text-center'>Register</div>
-            </Link>
-          </div>
-        </div>
-      </div>
-      <div className="flex w-full">
-        <div className="w-full flex items-center justify-center lg:w-1/2">
-          <div className='px-4 py-9 sm:px-6 sm:py-12 md:px-8 md:py-14 lg:px-10 lg:py-20 rounded-lg backdrop-blur bg-white/50 shadow-xl'>
-            <h1 className="font-extrabold text-center text-5xl sm:text-5xl md:text-5xl lg:text-5xl">
-              Create An Account
+    <div className="flex justify-center items-center h-screen bg-primary">
+      
+      <div className="flex w-full justify-center">
+        <div className="w-[900px] flex items-center justify-center">
+          <div className="w-full h-[650px] p-10 bg-primary shadow-2xl shadow-black/60">
+          <div className="flex flex-col lg:flex-row items-center gap-10 text-center">
+            <h1 className="font-Judson font-bold text-left text-[64px] text-[#FCF7D5]">
+              It’s Time <br /> to Take <br />Charge of<br /> Your <br />Stock
             </h1>
-            <p className='text-center font-medium text-lg sm:text-base md:text-lg lg:text-xl mt-4'>
-              Welcome to ConectaTec! Please enter your details.
+
+            <section className="text-left">
+            <h1 className="font-Judson font-bold text-center text-[48px] text-white">
+            Create An Account
+            </h1>
+    	    <p className='text-center font-Judson text-center text-[20px] text-white'>
+              Welcome to Smart Stock! Please enter your details.
             </p>
-            <div className="mt-8">
-              <div className="flex flex-row justify-center items-center">
-                <div className="py-1 px-2">
-                  <button
-                    className={`flex flex-row rounded-lg cursor-pointer py-2 px-4 bg-primary shadow-lg ${activeButton === 'student' ? 'bg-opacity-50' : ''}`}
-                    type="button"
-                    onClick={() => handleButtonClick('student')}
-                  >
-                    Student
-                    <div className='flex pt-1 px-1'>
-                      <svg className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"/>
-                      </svg>
-                    </div>
-                  </button>
-                </div>
-              </div>
-              <div className="mb-4">
-                <label className='px-4 text-lg font-medium'>Name</label>
-                <input
-                  className="w-full rounded-lg p-2 mt-1 bg-transparent shadow-lg"
+
+            <div className="mt-2">
+	      <div className="mb-2">
+                <label className=" ml-12 font-Judson block text-[36px] font-medium text-white mb-1">Name</label>
+                <div className='flex items-center justify-center'>
+                <input 
+                  className=" w-[500px] p-3 bg-[#ADBAC7] text-white font-Judson italic placeholder-gray-700 rounded-3xl"
                   placeholder="Enter your name"
-                  type="text"
-                />
+                  type="name"
+                /></div>
               </div>
+
               <div className="mb-4">
-                <label className='px-4 text-lg font-medium'>Email</label>
-                <input
-                  className="w-full rounded-lg p-2 mt-1 bg-transparent shadow-lg"
+                <label className=" ml-12 font-Judson block text-[36px] font-medium text-white mb-1">Email</label>
+                <div className='flex items-center justify-center'>
+                <input 
+                  className=" w-[500px] p-3 bg-[#ADBAC7] text-white font-Judson italic placeholder-gray-700 rounded-3xl"
                   placeholder="Enter your email"
                   type="email"
-                />
+                /></div>
               </div>
-              <div className="mb-4">
-                <label className='px-4 text-lg font-medium'>Password</label>
-                <input
-                  className="w-full rounded-lg p-2 mt-1 bg-transparent shadow-lg"
+              <div className="mb-6">
+                <label className=" ml-12 font-Judson block text-[36px] font-medium text-white mb-1">Password</label>
+                <div className='flex items-center justify-center'>
+                <input 
+                  className="w-[500px] p-3 bg-[#ADBAC7] text-white font-Judson italic placeholder-gray-700 rounded-3xl"
                   placeholder="Enter your password"
                   type="password"
-                />
+                /></div>
               </div>
-              
-              
-              <div className="flex justify-center items-center">
+
+              <div className="flex justify-center ">
                 <button
-                  className="cursor-pointer bg-primary hover:ease-in text-white py-2 px-4 rounded-lg font-semibold shadow-lg shadow-primary/50"
+                  className="w-[100px] h-12 bg-[#F7E67C] py-1 mt-5 text-[25px] text-primary font-suranna shadow-lg transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:bg-[#488ED3] duration-300 rounded-3xl"
                   type="submit"
                 >
                   Register
                 </button>
               </div>
-            </div>
+            </div></section>
           </div>
         </div>
+      </div>
       </div>
     </div>
   );
 };
+
 export default Register;

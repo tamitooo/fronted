8564cdc265117edd907a0904aae5fc0c9b997b4b
tamitooo <tamy.flores@utec.edu.pe>
2025-05-10 01:33:38 -0500

    logo

--- SmartStock/src/pages/Sidebar.tsx
+++ SmartStock/src/pages/Sidebar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import logo from "../images/logo.png";
+import logo from "../images/logo2.png";
 
 const Sidebar: React.FC = () => {
   const navigate = useNavigate();

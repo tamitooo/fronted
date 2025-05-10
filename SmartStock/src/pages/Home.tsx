00346d4801d3a06efd22bcf154ed91908cb0d86a
tamitooo <tamy.flores@utec.edu.pe>
2025-05-10 14:44:32 -0500

    home 100%

--- SmartStock/src/pages/Home.tsx
+++ SmartStock/src/pages/Home.tsx
@@ -7,13 +7,13 @@ const Home : React.FC = () => {
       <div className="flex flex-col lg:flex-row items-center gap-10 text-center">
         <img src={logo} alt="Logo" className="w-[373px] h-[357px] object-contain" />
         <section className="text-left">
-          <h1 className="font-Judson font-bold text-[128px] text-[#FCF7D5] leading-none">
+          <h1 className=" font-Judson font-bold text-[128px] text-[#FCF7D5] leading-none">
             Smart Stock
           </h1>
-          <p className="font-suranna text-[48px] text-[#FFFFFF] mt-4">
+          <p className="text-center font-suranna text-[48px] text-[#FFFFFF] mt-4">
             Turning Stock into Strategy
           </p>
-          <p className="font-Judson italic text-[32px] text-[#FFFFFF] mt-8">
+          <p className=" text-center font-Judson italic text-[32px] text-[#FFFFFF] mt-8">
             ✓ Real-Time Tracking – Know <br />
             exactly what’s in stock, 24/7.
             <br /><br />
